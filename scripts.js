9ebc1740930ee6347d360cb858a4f6a97f11d572
Samuel Pereira Barreto <70921394+samubarreto@users.noreply.github.com>
2023-10-22 19:08:17 -0300

    fixed box hover with !important

--- scripts.js
+++ scripts.js
@@ -1,88 +1,88 @@
-  let todoList = JSON.parse(localStorage.getItem('todoListStorage')) || [];
-  renderTodoList();
+let todoList = JSON.parse(localStorage.getItem('todoListStorage')) || [];
+renderTodoList();
 
-  function renderTodoList() {
-    let todoListHTML = '';
+function renderTodoList() {
+  let todoListHTML = '';
 
-    todoList.forEach((todoObject, index) => {
-      const { name, dueDate, selectedColor } = todoObject;
-      const html = `
-      <div class="div-gerada" style="background-color: ${selectedColor};">
-        <input type="checkbox" class="checkbox">
-        <p class="todo">${name}</p>
-        <p class="todo-date">${dueDate}</p>
-        <button class="remove-button" onclick="deleteTodo(${index})">remove</button>
-      </div>
-      `;
-      todoListHTML += html;
-    })
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate, selectedColor } = todoObject;
+    const html = `
+    <div class="div-gerada" style="background-color: ${selectedColor};">
+      <input type="checkbox" class="checkbox">
+      <p class="todo">${name}</p>
+      <p class="todo-date">${dueDate}</p>
+      <button class="remove-button" onclick="deleteTodo(${index})">remove</button>
+    </div>
+    `;
+    todoListHTML += html;
+  })
 
-    // for (let i = 0; i < todoList.length; i++) {
-    //   const todoObject = todoList[i];
-    //   const { name, dueDate, selectedColor } = todoObject;
-    //   const html = `
-    //     <div class="div-gerada" style="background-color: ${selectedColor};">
-    //       <input type="checkbox" class="checkbox">
-    //       <p class="todo">${name}</p>
-    //       <p class="todo-date">${dueDate}</p>
-    //       <button class="remove-button" onclick="deleteTodo(${i})">remove</button>
-    //     </div>
-    //   `;
-    //   todoListHTML += html;
-    // }
+  // for (let i = 0; i < todoList.length; i++) {
+  //   const todoObject = todoList[i];
+  //   const { name, dueDate, selectedColor } = todoObject;
+  //   const html = `
+  //     <div class="div-gerada" style="background-color: ${selectedColor};">
+  //       <input type="checkbox" class="checkbox">
+  //       <p class="todo">${name}</p>
+  //       <p class="todo-date">${dueDate}</p>
+  //       <button class="remove-button" onclick="deleteTodo(${i})">remove</button>
+  //     </div>
+  //   `;
+  //   todoListHTML += html;
+  // }
 
-    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-  }
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
 
-  function addTodo() {
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
 
-    const dateInputElement = document.querySelector('.js-date-input');
-    let dueDate = dateInputElement.value;
+  const dateInputElement = document.querySelector('.js-date-input');
+  let dueDate = dateInputElement.value;
 
-    const colorInputElement = document.querySelector('.js-color-input')
-    const selectedColor = colorInputElement.value;
+  const colorInputElement = document.querySelector('.js-color-input')
+  const selectedColor = colorInputElement.value;
 
-    if (name != '') {
-      
-      document.querySelector('.js-advice').innerHTML = ''
-      
-      if (dueDate.length === 0) {
-        dueDate = 'Ø'
-      }
-
-      todoList.push({
-        name,
-        dueDate,
-        selectedColor
-      });
+  if (name != '') {
     
-      inputElement.value = '';
-      dateInputElement.value = '';
+    document.querySelector('.js-advice').innerHTML = ''
     
-      localStorage.setItem('todoListStorage', JSON.stringify(todoList));
-    
-      renderTodoList();
-    } else {
-      document.querySelector('.js-advice').innerHTML = `<div class="js-advice" style="
-      border: none;
-      background-color: red;
-      padding: 8px 30px;
-      border-radius: 50px;
-      animation: float ease 3s;
-      ">Preencha o primeiro campo</div>`
+    if (dueDate.length === 0) {
+      dueDate = 'Ø'
     }
-  }
 
-  function deleteTodo(index) {
-    todoList.splice(index, 1);
+    todoList.push({
+      name,
+      dueDate,
+      selectedColor
+    });
+  
+    inputElement.value = '';
+    dateInputElement.value = '';
+  
     localStorage.setItem('todoListStorage', JSON.stringify(todoList));
+  
     renderTodoList();
+  } else {
+    document.querySelector('.js-advice').innerHTML = `<div class="js-advice" style="
+    border: none;
+    background-color: red;
+    padding: 8px 30px;
+    border-radius: 50px;
+    animation: float ease 3s;
+    ">Preencha o primeiro campo</div>`
   }
+}
 
-  function detectKeydownEnter(event) {
-    if (event.key === 'Enter') {
-      addTodo()
-    }
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  localStorage.setItem('todoListStorage', JSON.stringify(todoList));
+  renderTodoList();
+}
+
+function detectKeydownEnter(event) {
+  if (event.key === 'Enter') {
+    addTodo()
   }
+}
